50b09f32df3e93816d4ea2247b218fb8d3a21b17
Ethan Tang <itsethang5@gmail.com>
2024-05-13 01:27:25 -0400

    Built an API create adding snacks

---
+++ app/api/snacks/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createSnackSchema = z.object({
+  image: z.string().url({ message: "Invalid url" }).max(2048),
+  name: z
+    .string()
+    .min(1, { message: "Name cannot be empty!" })
+    .max(255, { message: "Name is too long!" }),
+  description: z.string().min(1, { message: "Description cannot be empty" }),
+  price: z.number().nonnegative({ message: "Price cannot be negative!" }),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createSnackSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newSnack = await prisma.snack.create({
+    data: {
+      image: body.image,
+      name: body.name,
+      description: body.description,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newSnack, { status: 201 });
+}
