a2e76ea8173c5b10f8f79baddf69646d2568deb5
Ethan Tang <itsethang5@gmail.com>
2024-06-02 00:15:37 -0400

    Create delete message popup

--- app/snacks/[id]/DeleteSnackButton.tsx
+++ app/snacks/[id]/DeleteSnackButton.tsx
@@ -1,13 +1,37 @@
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 import { FaTrashCan } from "react-icons/fa6";
 
 const DeleteSnackButton = ({ snackId }: { snackId: number }) => {
   return (
-    <Button color="crimson">
-      <FaTrashCan />
-      Delete Snack
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="crimson">
+          <FaTrashCan />
+          Delete Snack
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Delete Snack</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This snack will be permanently deleted from Amazing
+          Snack Shop's databse. You cannot undo this action.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              No, let me think...
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="crimson">
+              Yes. I want to delete it!
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
