352bae2c6e74af2cd83641e0bd6bfbb07af0fc87
Ethan Tang <itsethang5@gmail.com>
2024-06-03 22:17:32 -0400

    Add My Order icon to navbar and My Order page

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,7 +3,9 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { GiChipsBag } from "react-icons/gi";
+import { FaShoppingBag } from "react-icons/fa";
 import classNames from "classnames";
+import { Flex, Text } from "@radix-ui/themes";
 
 const Navbar = () => {
   const links = [
@@ -14,7 +16,7 @@ const Navbar = () => {
   const activePath = usePathname();
 
   return (
-    <nav className="flex space-x-6 border-b-2 mb-5 px-4 h-16 items-center">
+    <nav className="flex border-b-2 mb-5 px-6 h-16 items-center justify-between">
       <Link href="/">
         <GiChipsBag className="h-auto w-8" />
       </Link>
@@ -35,6 +37,13 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <Link
+        href="/snacks/orders"
+        className="hover:underline underline-offset-8"
+      >
+        <FaShoppingBag className="h-auto w-5 inline mr-2" />
+        <Text>My Order</Text>
+      </Link>
     </nav>
   );
 };
---
+++ app/snacks/orders/page.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "@radix-ui/themes";
+import React from "react";
+
+const SnackOrderPage = () => {
+  return <Heading>My Order</Heading>;
+};
+
+export default SnackOrderPage;
