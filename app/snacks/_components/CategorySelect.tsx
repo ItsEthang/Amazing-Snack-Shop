56dad1436e7b5fb2bbee6db23dc67a4668a1b87e
Ethan Tang <itsethang5@gmail.com>
2024-07-04 01:05:10 -0400

    Fetch categories from backend db with React Query

--- app/snacks/_components/CategorySelect.tsx
+++ app/snacks/_components/CategorySelect.tsx
@@ -1,27 +1,46 @@
 "use client";
 
+import { Category } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Skeleton } from "@/app/components";
 
 const CategorySelect = () => {
+  const { data: categories, error, isLoading } = useCategory();
+
+  if (isLoading) return <Skeleton />;
+
+  if (error) return null;
   return (
     <>
       <Select.Root>
-        <Select.Trigger placeholder="Select Category" />
+        <Select.Trigger placeholder="Assign Category" />
         <Select.Content>
           <Select.Group>
             <Select.Label>Category Options</Select.Label>
             <Select.Item value="1">Uncategorized</Select.Item>
             <Select.Item value="7">Packaged Food</Select.Item>
-            {/* {users?.map((user) => (
-              <Select.Item key={user.id} value={user.id}>
-                {user.name}
+            {categories?.map((category) => (
+              <Select.Item key={category.id} value={`${category.id}`}>
+                {category.name}
               </Select.Item>
-            ))} */}
+            ))}
           </Select.Group>
         </Select.Content>
       </Select.Root>
     </>
   );
+};
+
+const useCategory = () => {
+  return useQuery({
+    queryKey: ["categories"],
+    queryFn: () =>
+      axios.get<Category[]>("/api/categories").then((res) => res.data),
+    staleTime: 600_000,
+    retry: 3,
+  });
 };
 
 export default CategorySelect;
