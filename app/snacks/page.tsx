b188e9b2d4d25d05724f35d409dcaca496ff3ffd
Ethan Tang <itsethang5@gmail.com>
2024-09-09 00:15:47 -0400

    Snack Page refactoring

--- app/snacks/page.tsx
+++ app/snacks/page.tsx
@@ -6,6 +6,7 @@ import SnackImage from "../components/SnackImage";
 import SnackToolBar from "./SnackToolBar";
 import { Snack } from "@prisma/client";
 import Pagination from "../components/Pagination";
+import SnackGrid from "./SnackGrid";
 
 export interface QueryType {
   category: string;
@@ -57,44 +58,7 @@ const SnacksPage = async ({ searchParams }: Props) => {
   return (
     <div>
       <SnackToolBar query={searchParams} />
-      <Grid columns={{ initial: "1", xs: "2", md: "3", lg: "5" }} gap="5">
-        {snacks.map((snack) => (
-          <Box
-            key={snack.id}
-            className="border-solid border-2 border-zinc-200 rounded-lg p-5 "
-          >
-            <Flex direction="column" gap="3">
-              <SnackStockBadge quantity={snack.quantity} />
-              <Link
-                href={`/snacks/${snack.id}`}
-                key={snack.id}
-                className="hover:text-slate-400"
-              >
-                <Box
-                  position="relative"
-                  width="100%"
-                  height={{ initial: "300px", md: "250px", lg: "200px" }}
-                >
-                  <SnackImage url={snack.image} alt={snack.name} />
-                </Box>
-                <Text
-                  as="div"
-                  align="center"
-                  className="overflow-hidden text-ellipsis whitespace-nowrap"
-                >
-                  {snack.name.toUpperCase()}
-                </Text>
-                <Flex justify="between">
-                  <Text as="div">Price: </Text>
-                  <Text as="div">${+snack.price}</Text>
-                </Flex>
-              </Link>
-
-              <AddToCart product={snack} />
-            </Flex>
-          </Box>
-        ))}
-      </Grid>
+      <SnackGrid snacks={snacks} />
       <Pagination itemCtn={itemCtn} pageSize={pageSize} currPage={currPage} />
     </div>
   );
