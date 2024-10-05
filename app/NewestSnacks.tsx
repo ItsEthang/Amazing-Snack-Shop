55f14e68bf754f823d5317d8a82a7a67d65083bf
Ethan Tang <itsethang5@gmail.com>
2024-10-05 18:07:42 -0400

    Use custom hook to fetch newest snacks

--- app/NewestSnacks.tsx
+++ app/NewestSnacks.tsx
@@ -1,13 +1,16 @@
+"use client";
+
 import prisma from "@/prisma/client";
 import { Box, Grid, Text } from "@radix-ui/themes";
 import React from "react";
 import SnackBoxDashboard from "./components/SnackBoxDashboard";
+import { useNewestSnack } from "./hooks/useNewestSnack";
+import { Skeleton } from "@/app/components";
 
-const NewestSnacks = async () => {
-  const newSnacks = await prisma.snack.findMany({
-    orderBy: { addedOn: "desc" },
-    take: 4,
-  });
+const NewestSnacks = () => {
+  const { data: snacks, error, isLoading } = useNewestSnack();
+  if (isLoading) return <LoadingSnacks />;
+  if (error) return null;
   return (
     <Box className="border-solid border-2 border-zinc-200 p-5 rounded-lg">
       <Box className="text-center mb-3">
@@ -16,9 +19,34 @@ const NewestSnacks = async () => {
         </Text>
       </Box>
 
+      {error && (
+        <Text>
+          Sorry, but we cannot find the newest snacks at the moment. :(
+        </Text>
+      )}
+
       <Grid columns={{ initial: "2" }} gap="5">
-        {newSnacks.map((snack) => (
+        {snacks?.map((snack) => (
           <SnackBoxDashboard key={snack.id} snack={snack} />
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+const LoadingSnacks = () => {
+  return (
+    <Box className="border-solid border-2 border-zinc-200 p-5 rounded-lg">
+      <Box className="text-center mb-3">
+        <Text weight="bold" size={{ initial: "5", sm: "8" }}>
+          NEWEST SNACKS
+        </Text>
+      </Box>
+      <Grid columns={{ initial: "2" }} gap="5">
+        {Array.from({ length: 4 }, (_, index: number) => (
+          <div key={index}>
+            <Skeleton width="100%" height="12rem" />
+          </div>
         ))}
       </Grid>
     </Box>
--- app/hooks/useNewestSnack.ts
+++ app/hooks/useNewestSnack.ts
@@ -1,12 +1,12 @@
-import { Category } from "@prisma/client";
+import { Snack } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-export const useCategory = () =>
+export const useNewestSnack = () =>
   useQuery({
     queryKey: ["newestSnacks"],
     queryFn: () =>
-      axios.get<Category[]>("/api/snacks/newest").then((res) => res.data),
+      axios.get<Snack[]>("/api/snacks/newest").then((res) => res.data),
     staleTime: 300_000,
     retry: 3,
   });
